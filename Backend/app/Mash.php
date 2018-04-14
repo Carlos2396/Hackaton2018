3f0a028979bda96da7a5dbc3695ced2011da2f4e
Estefania Pitol Martinez <A01551688@itesm.mx>
2018-04-14 03:58:06 -0500

    Mash controller

--- Backend/app/Mash.php
+++ Backend/app/Mash.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mash extends Model
 {
+    static $keys = ['Do', 'Re', 'Mi', 'Fa', 'Sol', 'La', 'Si'];
+    static $metres = ['3/4', '4/4', '6/8', '7/8', '5/4'];
     /**
      * The attributes that are mass assignable.
      *
@@ -14,6 +16,27 @@ class Mash extends Model
     protected $fillable = [
         'name', 'start_datetime', 'quantum', 'bpm', 'key', 'metre', 'user_id'
     ];
+
+    //Static methods
+    public static function matchesKey($input)
+    {
+        foreach ($keys as $key)
+        {
+            if(strcmp($key, $input) == 0)
+                return true;
+        }
+        return false;
+    }
+
+    public static function matchesMetre($input)
+    {
+        foreach ($metres as $metre)
+        {
+            if(strcmp($metre, $input) == 0)
+                return true;
+        }
+        return false;
+    }
 
     protected $dates = [
         'start_datetime'
