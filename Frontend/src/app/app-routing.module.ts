78982679a97103bd6bb8f75caed243966f1427cf
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-04-14 00:07:28 -0500

    Changes to create-snippet

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { AuthGuard } from './guards/auth.guard';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { LoginComponent } from './components/authentication/login/login.component'
+import { LoginComponent } from './components/authentication/login/login.component';
+import { CreateSnippetComponent } from './components/snippet/create-snippet/create-snippet.component'
 const routes: Routes = [
 
     // General
     { path:'', component: LoginComponent},
+
+    //Snippets
+    {path:'snippets/create', component: CreateSnippetComponent, canActivate: [AuthGuard]},
 
   ];
   
--- Frontend/src/app/components/snippet/create-snippet/create-snippet.component.ts
+++ Frontend/src/app/components/snippet/create-snippet/create-snippet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-snippet',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSnippetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService, private router:Router) { }
 
   ngOnInit() {
   }
