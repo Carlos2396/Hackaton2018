2be83692ddf17bfac1acd15143f09ee7ec3f47eb
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-04-14 08:08:59 -0500

    Mistakes in frontend models were corrected.

--- Frontend/src/app/components/mash/mash-create/mash-create.component.ts
+++ Frontend/src/app/components/mash/mash-create/mash-create.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from '../../../models/user.model';
-import { Snippet } from '../../../models/snippet.model';
+import { Mash } from '../../../models/mash.model';
 import { CrudService } from '../../../services/crud.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -15,12 +15,14 @@ export class MashCreateComponent implements OnInit {
 
   message: string;
   user: User;
-  snippet: Snippet;
+  mash: Mash;
 
   constructor(private auth:AuthService, private router:Router) { }
 
   ngOnInit() {
-    
+    this.mash = new Mash(null, null, null, null, null, null, null, null, null, null, null, null, null);
+    let user_id = this.auth.getMember().id;
+    this.mash.user_id = user_id;
   }
 
 }
--- Frontend/src/app/models/snippet.model.ts
+++ Frontend/src/app/models/snippet.model.ts
@@ -4,11 +4,11 @@ import { Round } from "./round.model"
 
 export class Snippet {
     constructor(
-        public updated_at: Date,
-        public created_at: Date,
         public name: string,
         public instrument_id: number,
         public user_id: number,
+        public updated_at?: Date,
+        public created_at?: Date,
         public instrument?: Instrument,
         public user?: User,
         public rounds?: Round[],
