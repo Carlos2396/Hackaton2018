b2158afe26da6728053fb008c059ce8091e25dea
Angel Roberto Ruiz Mendoza <angel.r.ruiz.m@gmail.com>
2018-04-14 14:02:23 -0500

    merging

--- Frontend/src/app/components/mash/mash-list/mash-list.component.ts
+++ Frontend/src/app/components/mash/mash-list/mash-list.component.ts
@@ -6,6 +6,7 @@ import { Mash } from '../../../models/mash.model';
 import { Round } from '../../../models/round.model';
 import { Snippet } from '../../../models/snippet.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 
 @Component({
@@ -15,11 +16,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class MashListComponent implements OnInit {
 
+  successMessage: string;
   message: string;
   mashes: Mash[];
   mashesDest: Mash[];
 
-  constructor(private crud:CrudService, private router:Router) { }
+  constructor(private crud:CrudService, private router:Router,  private auth:AuthService) { }
 
   ngOnInit() {
     this.message = '';
@@ -47,5 +49,49 @@ export class MashListComponent implements OnInit {
     )
   }
 
+  createMashUser(id: number){
+    var user_id = this.auth.getUser().id;
+    let x ={
+      mash_id: id,
+      user_id: user_id
+    }
 
+    this.crud.create(this.crud.models.MASH_USER, x)
+    .subscribe(
+      (res)=>{
+        console.log(res);
+        this.successMessage = 'Le diste like sin errores'
+      },
+      (err:HttpErrorResponse) => {
+        if(err.error){
+          this.message = err.error.message;
+        }
+        else{
+          this.message = err.error.errors[0].message;
+        }
+      }
+    )
+
+  }
+
+  removeMessage(){
+    this.successMessage = '';
+    this.message = '';
+  }
+
+
+  playAll(){
+    
+    console.log("Playing");
+    var audio1 = new Pz.Sound('../../../assets/music/bensound-cute.mp3', function(){
+      var audio2 = new Pz.Sound('../../../../../assets/music/bensound-jazzyfrenchy.mp3', function(){
+        var group = new Pz.Group();
+        
+        group.addSound(audio1);
+        group.addSound(audio2);
+        group.play();
+      });
+    });
+    
+  }
 }
--- Frontend/src/app/components/mash/mash-retrieve/mash-retrieve.component.ts
+++ Frontend/src/app/components/mash/mash-retrieve/mash-retrieve.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, group } from '@angular/core';
+import * as Pz from "pizzicato";
 import { CrudService } from '../../../services/crud.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { User } from '../../../models/user.model';
@@ -122,5 +123,20 @@ export class MashRetrieveComponent implements OnInit {
 
   addSnippet(){
     this.router.navigate(['mash/'+this.mash.rounds.length + '/snippet']);
+  }
+
+  playAll(){
+    
+    console.log("Playing");
+    var audio1 = new Pz.Sound('../../../assets/music/bensound-cute.mp3', function(){
+      var audio2 = new Pz.Sound('../../../../../assets/music/bensound-jazzyfrenchy.mp3', function(){
+        var group = new Pz.Group();
+        
+        group.addSound(audio1);
+        group.addSound(audio2);
+        group.play();
+      });
+    });
+    
   }
 }
